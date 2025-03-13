c26756cd9b8158ae44d1687f4f1402f67f4167da
samsonchang2028 <schan119@calpoly.edu>
2025-03-13 00:35:02 -0700

    first commit

---
+++ scroller.js
@@ -0,0 +1,36 @@
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+function scrollToSection2() {
+  const section2 = document.getElementById("section-3");
+  const offset = 100; // Adjust this value if you want an offset (e.g., for a fixed header)
+  const section2Position = section2.offsetTop - offset;
+
+  window.scrollTo({
+      top: section2Position,
+      behavior: "smooth" // Enable smooth scrolling
+  });
+}
